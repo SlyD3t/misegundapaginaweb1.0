45d41f579a80c8b91f6f0d00ee658977d8d557b5
Veronica Rivas <veronica.rodriguez2@catolica.edu.sv>
2026-09-16 20:07:48 -0600

    mi primer commit

---
+++ main.js
@@ -0,0 +1,18 @@
+numero=5;
+frase="Hola mundo";
+boleano=true;
+doble=5.5;
+alert("Hola mundo");
+let elemento= document.querySelector('div');
+elemento.style.backgroundColor= 'White';
+elemento.style.color='Black';
+
+console.log("Valor de la variable:"+ numero+" El tipo de la variable es:"+ typeof numero);
+console.log("Valor de la variable:"+ frase+" El tipo de la variable es:"+ typeof frase);
+console.log("Valor de la variable:"+ boleano+" El tipo de la variable es:"+ typeof boleano);
+console.log("Valor de la variable:"+ doble+" El tipo de la variable es:"+ typeof doble);
+
+/*let elementohtml= document.querySelector('body');*/
+/*elementohtml.textContent="El tipo de variable es: "+ typeof frase+ " Y el contenido es: "+ frase;*/
+elemento.textContent="El tipo de variable es: "+ typeof frase+ " Y el contenido es: "+ frase;
+
