b68c919cd43a30df84db9b654237d959ebd2945f
Veronica Rivas <veronica.rodriguez2@catolica.edu.sv>
2026-09-16 20:16:27 -0600

    modifique los 3 archivos para ver si la conexion entre github va excelente

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ numero=5;
 frase="Hola mundo";
 boleano=true;
 doble=5.5;
-alert("Hola mundo");
+alert("Hola a todos los que estan viendo mi repositorio de GitHub, bienvenidos a mi segundo commit");
 let elemento= document.querySelector('div');
 elemento.style.backgroundColor= 'White';
 elemento.style.color='Black';
 
-console.log("Valor de la variable:"+ numero+" El tipo de la variable es:"+ typeof numero);
+console.log("He agragado esta linea nada mas para comprobar que si funciona el commit");
 console.log("Valor de la variable:"+ frase+" El tipo de la variable es:"+ typeof frase);
 console.log("Valor de la variable:"+ boleano+" El tipo de la variable es:"+ typeof boleano);
 console.log("Valor de la variable:"+ doble+" El tipo de la variable es:"+ typeof doble);
